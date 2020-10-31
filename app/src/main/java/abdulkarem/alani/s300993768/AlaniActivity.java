2c710f70120c78bd645c41f3abfe7dbff985b0ca
Karem <karemalani2402@yahoo.com>
2020-10-31 08:53:09 -0400

    First Comit

---
+++ app/src/main/java/abdulkarem/alani/s300993768/AlaniActivity.java
@@ -0,0 +1,14 @@
+package abdulkarem.alani.s300993768;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AlaniActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alani);
+    }
+}
